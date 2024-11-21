964721a1a12787b48ec7a918c1b6f48f976bd7a9
HeitorLM <heitor_lm@hotmail.com>
2024-11-21 11:45:50 -0300

    feat: adiciona funções para recuperar locations e users

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { OccurrenceDB } from '../types/occurrence';
+import { OccurrenceDB, LocationDB, UsersEntity } from '../types';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://192.168.15.181:3001'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
 });
 
 export const getOccurrences = async (): Promise<OccurrenceDB[]> => {
@@ -16,6 +16,27 @@ export const getOccurrences = async (): Promise<OccurrenceDB[]> => {
         return data;
     } catch (error) {
         console.error('Erro detalhado ao buscar ocorrências:', error);
+        throw error;
+    }
+};
+
+export const getLocations = async (): Promise<LocationDB[]> => {
+    try {
+        const { data } = await api.get<LocationDB[]>('/locations');
+        return data;
+    } catch (error) {
+        console.error('Erro detalhado ao buscar localizações:', error);
+        throw error;
+    }
+};
+
+export const getUsers = async (): Promise<UsersEntity[]> => {
+    try {
+        const { data } = await api.get<UsersEntity[]>('/users');
+
+        return data;
+    } catch (error) {
+        console.error('Erro detalhado ao buscar usuários:', error);
         throw error;
     }
 };
