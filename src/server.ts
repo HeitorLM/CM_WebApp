5c1d9b784a3455d521c6fca24e876fe7fc42ff72
HeitorLM <heitor_lm@hotmail.com>
2025-01-06 01:49:07 -0300

    feat: adiciona server express simples para servir o app react

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Serve os arquivos estáticos do build do React
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
