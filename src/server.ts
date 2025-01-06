69d8b7d929abfc24a351162ef8f931aca1b53c4e
HeitorLM <heitor_lm@hotmail.com>
2025-01-06 02:48:53 -0300

    fix: modifica variável de env da porta do server

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
-
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.WEB_API_PORT || 3001;
 
 // Serve os arquivos estáticos do build do React
 app.use(express.static(path.join(__dirname, '..', 'build')));
