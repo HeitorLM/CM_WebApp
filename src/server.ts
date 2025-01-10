a0bf1cc887450b7617f036529a7a37366c1f1f21
HeitorLM <heitor_lm@hotmail.com>
2025-01-10 04:56:01 -0300

    feat: adiciona leitura correta das variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import path from 'path';
+import * as dotenv from "dotenv";
 
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
+// Configurações de ambiente
+dotenv.config({
+  path: fileURLToPath(new URL("../.env", import.meta.url)),
+});
+
 const app = express();
-const port = process.env.WEB_API_PORT || 3001;
+const port = process.env.WEB_API_PORT || 4000;
 
 // Serve os arquivos estáticos do build do React
 app.use(express.static(path.join(__dirname, '..', 'build')));
