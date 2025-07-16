d5e451086c7da35d9043bfb6aff3890496d21a0a
HeitorLM <heitor_lm@hotmail.com>
2025-07-16 18:06:17 -0300

    feat: adiciona suporte a CORS e atualiza configuração do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import * as dotenv from "dotenv";
 
 import { fileURLToPath } from 'url';
@@ -12,16 +13,22 @@ dotenv.config({
   path: fileURLToPath(new URL("../.env", import.meta.url)),
 });
 
+
 const app = express();
-const port = process.env.WEB_API_PORT || 4000;
+const BASE_URL = process.env.VITE_API_BASE_URL || "localhost";
+const PORT = process.env.VITE_API_PORT || 5000;
+
+// Use CORS middleware
+app.use(cors());
 
 // Serve os arquivos estáticos do build do React
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+// Rota para a página inicial
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${BASE_URL}:${PORT}`);
 });
