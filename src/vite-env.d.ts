2d31214b0c032bb6d8d3485543318567c88b5947
HeitorLM <heitor_lm@hotmail.com>
2025-07-18 15:57:11 -0300

    feat: atualiza definições de tipos para variáveis de ambiente do Express

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
+  readonly VITE_EXPRESS_BASE_URL: string
+  readonly VITE_EXPRESS_PORT: string
   readonly VITE_API_PORT: string
 }
 
